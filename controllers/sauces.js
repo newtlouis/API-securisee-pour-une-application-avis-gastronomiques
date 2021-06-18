950898882e55d8426f09d4dd8e9026cda572052d
Louis <louis.m.newton@hotmail.fr>
2021-06-18 21:41:03 +0200

    100% controllers et routes api/sauce

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -7,7 +7,31 @@ exports.createSauce = (req,res,next) => {
         ...req.body
     });
     thing.save()
-    .then(() => res.status(201).json({message: "Utilisateur enregistré"}))
+    .then(() => res.status(201).json({message: "Sauce enregistrée"}))
     .catch(err => res.status(400).json({err}));
 };
+
+exports.showAllSauces = (req,res,next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch (err => res.status(404).json({err}))
+};
+
+exports.deleteSauce = (req,res,next) => {
+    Thing.deleteOne({_id: req.params.id})
+  .then(() => res.status(200).json({message: 'Sauce suprimée'}))
+  .catch(err => res.status(400).json({err}));
+};
+
+exports.updateSauce = (req,res,next) => {
+    Thing.updateOne({_id: req.params.id},{...req.body,_id: req.params.id})
+.then(() => res.status(200).json({message: 'Sauce modifiée'}))
+.catch(err => res.status(400).json({err}));
+};
+
+exports.showSauce = (req,res,next) => {
+    Thing.findOne({_id : req.params.id})
+    .then(thing => res.status(200).json(thing))
+    .catch( err => res.status(404).json({err}));
+}
 
