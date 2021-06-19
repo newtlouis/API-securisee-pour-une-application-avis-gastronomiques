d8f04c3da2a663e073100b8010c18313fd0048b3
Louis <louis.m.newton@hotmail.fr>
2021-06-19 17:17:31 +0200

    fonction signup avec hashage Bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,17 @@ const User = require ('../models/User.js');
 
 
 exports.signup = (req,res,next) => {
-
+    bcrypt.hash(req.body.password,10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                paswword: hash
+            });
+            user.save()
+                .then(()=> res.status(201).json({message:'Bienvenue, votre compte à bien été crée.'}))
+                .catch(err => res.status(400).json({err}))
+        })
+        .catch(err => res.status(500).json({err}));
 };
 
 exports.login = (req,res,next) => {
