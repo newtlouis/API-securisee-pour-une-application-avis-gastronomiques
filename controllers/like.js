03f41b2004a61ae5304a94fd3eb69e88190de63d
Louis <louis.m.newton@hotmail.fr>
2021-06-25 12:40:20 +0200

    disliked

--- controllers/like.js
+++ controllers/like.js
@@ -3,7 +3,7 @@ const Thing = require("../models/Thing");
 exports.like = (req,res,next) => {
     // Récuptération Id sauce
     idSauce = req.baseUrl.split("/")[3];
-    console.log(idSauce);
+    // console.log(idSauce);
 
                         
             // Si c'est un like
@@ -13,14 +13,26 @@ exports.like = (req,res,next) => {
                         $push:{usersLiked: req.body.userId},
                         $inc : {likes : +1}
                     }
+                  
                     )
-                .then(() => res.status(200).json({message : 'sauce aimée'}))
+                .then(() => res.status(200).json({message : 'sauce aimée'})                        )
                 .catch(err => res.status(400).json({err}))
                 }    
                 
-            // Si c'est un like
+            // Si c'est un dislike
             if (req.body.like == -1) {
-                Thing.updateOne({_id : req.params.id},
+                Thing.updateOne({_id : idSauce},
+                    {
+                        $push:{usersDisliked: req.body.userId},
+                        $inc : {dislikes : +1}
+                    }
+                    )
+                .then(() => res.status(200).json({message : 'sauce aimée'}))
+                .catch(err => res.status(400).json({err}))
+                }
+            // Si c'est une annulation
+            if (req.body.like == 0) {
+                Thing.updateOne({_id : idSauce},
                     {
                         $push:{arrayUsersDisliked: req.body.userId},
                         $inc : {dislikes : +1}
